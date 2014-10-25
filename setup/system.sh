42450a44c67b7afa2ff60abe0f3bd6e6f5834d3e
Damian Kaczmarek <rush@rushbase.net>
2014-10-25 23:35:11 +0200

    Add system configuration (without iso download yet)

---
+++ setup/system.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if ! [ -e .system-setup ];then
+	if ! [ -e ~/.ssh/id_rsa.pub ];then
+		ssh-key-gen -b 4096
+	fi
+	echo 'I am about to create user "virtkick" in group "kvm" and add your public ssh key to allow passwordless login'
+	sudo bash -c "
+	groupadd kvm 2> /dev/null;
+	useradd virtkick -m -c \"VirtKick orchestrator\" -g kvm && 
+	mkdir -p ~virtkick/{.ssh,hdd,iso} &&
+	chown -R virtkick:kvm ~virtkick &&
+	chmod 750 ~virtkick &&
+	cat >> ~virtkick/.ssh/authorized_keys" < ~/.ssh/id_rsa.pub
+	touch .system-setup
+fi
