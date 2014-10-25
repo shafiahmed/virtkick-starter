ba2aeb29738026f53e757dbe7728ce560ffe9ca6
Damian Kaczmarek <rush@rushbase.net>
2014-10-25 23:34:56 +0200

    Fix indent

--- setup/python.sh
+++ setup/python.sh
@@ -6,14 +6,14 @@ if ! [ -e "$PYENV_ROOT/installed" ];then
 fi
 
 if ! [ -e "$PYENV_ROOT" ];then
-  git clone git://github.com/yyuu/pyenv.git .pyenv
-  pyenv install 2.7.6
-  pyenv rehash
-  git clone https://github.com/yyuu/pyenv-virtualenv.git .pyenv/plugins/pyenv-virtualenv
-  pyenv virtualenv 2.7.6 virtkick
-  eval "$(pyenv init -)"
-  pyenv activate virtkick
-  cd .pyenv
+	git clone git://github.com/yyuu/pyenv.git .pyenv
+	pyenv install 2.7.6
+	pyenv rehash
+	git clone https://github.com/yyuu/pyenv-virtualenv.git .pyenv/plugins/pyenv-virtualenv
+	pyenv virtualenv 2.7.6 virtkick
+	eval "$(pyenv init -)"
+	pyenv activate virtkick
+	cd .pyenv
 	wget ftp://xmlsoft.org/libxml2/libxml2-2.7.2.tar.gz
 	tar -xzvf libxml2-2.7.2.tar.gz
 	cd libxml2-2.7.2/
@@ -21,9 +21,9 @@ if ! [ -e "$PYENV_ROOT" ];then
 	make -j4
 	cd python/
 	python setup.py install
-  cd ../../..
-  pip install libvirt-python==$LIBVIRT_VERSION
-  touch "$PYENV_ROOT/installed"
+	cd ../../..
+	pip install libvirt-python==$LIBVIRT_VERSION
+	touch "$PYENV_ROOT/installed"
 else
 	eval "$(pyenv init -)"
 	pyenv activate virtkick
