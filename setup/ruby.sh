35ab238cf4821c6668b8f9787fcbf6982c1a60d6
Damian Kaczmarek <rush@rushbase.net>
2014-10-26 02:53:35 +0100

    Fix ruby setup

--- setup/ruby.sh
+++ setup/ruby.sh
@@ -11,12 +11,15 @@ if [ ! -d "$RVM_DIR" ];then
 	export rvm_path="$RVM_DIR"
 	curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles --autolibs=rvm_pkg
 	. .rvm/scripts/rvm
+  export PATH="$RVM_DIR/bin:$PATH"
 	rvm install 2.1.3
 	rvm alias create default 2.1.3
   touch "$RVM_DIR/installed"
+else
+	. .rvm/scripts/rvm
+	export PATH="$RVM_DIR/bin:$PATH"
 fi
-. .rvm/scripts/rvm
-export PATH="$RVM_DIR/bin:$PATH"
+
 
 
 
